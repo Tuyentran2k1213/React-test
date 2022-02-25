dda38c9528ea744b3d7b6e2c2a5536ec232e6404
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-02-26 00:07:21 +0700

    react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+// const gifts = [
+//     'cpu i9',
+//     'RAM 32GB RGB',
+//     'RGB Keyboard'
+// ]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//   const [infor, setInfor] = useState({
+//     name: 'Tuyn',
+//     age: 18,
+//   });
+// const handleClick = () => {
+//   setInfor(huhu => ({...huhu, bio: 'oke'}));
+// };
+
+//   return (
+//     <div className="App" style={{paddingLeft: 50}}>
+//       <h1>{JSON.stringify(infor)}</h1>
+//       <button onClick = {handleClick}>update</button>
+//     </div>
+//   );
+
+// const [gift, setGift] = useState();
+
+// const handleClick = () => {
+//     let rand = Math.floor(Math.random() * gifts.length);
+//     setGift(gifts[rand]);
+// }
+// return (
+//     <div className="App" style={{paddingLeft: 50}}>
+//        <h1>{gift || 'Chưa có phần thưởng'}</h1>
+//        <button onClick = {handleClick}>Nhận thưởng</button>
+//      </div>
+// )
+
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+
+// const handleClick = () => {
+//   console.log({
+//     name,
+//     email
+//   })
+// }
+
+// return (
+//     <div className="App" style={{paddingLeft: 50}}>
+//        <input
+//        value={name} 
+//        onChange={e => {setName(e.target.value)}}/>
+//        <input
+//        value={email} 
+//        onChange={e => {setEmail(e.target.value)}}/>
+//        <button onClick={handleClick}>register</button>
+//        <p>{name}</p>
+//      </div>
+// )
+
+const courses = [
+  {
+    id: '1',
+    name: 'HTML, javaScipt'
+  },
+  {
+    id: '2',
+    name: 'RUBY, JAVA'
+  },
+  {
+    id: '3',
+    name: 'PHP, C++'
+  },
+]
+
+// const [checked, setCheked] = useState();
+
+
+// return (
+//   <div style={{paddingLeft: 50}}>
+//     {courses.map((course) => (
+//       <div key={course.id}>
+//         <input
+//         type='checkbox'
+//         checked = {checked === course.id}
+//         onChange={() => {setCheked(course.id)}}
+//         />{course.name}
+//       </div>
+//     ))}
+//   </div>
+// )
+
+const [checked, setCheked] = useState([]);
+
+const isCheck = (value) => checked.includes(value);
+
+const handlingChange = (id) => {
+  setCheked(prev => {
+    const isChecked = isCheck(id);
+    if(isChecked){
+      return checked.filter((check) => check != id);
+    }
+    if(!isChecked){
+      return [...prev, id]
+    }
+  });
 }
+
+const handlingSubmit = () => {
+  if(checked.length){
+    console.log(checked);
+  }
+}
+
+  return (
+    <div style = {{padding: 50}}>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type='checkbox'
+            checked={isCheck(course.id)}
+            onChange={() => handlingChange(course.id)}
+          />{course.name}
+          </div>
+      ))}
+      <button onClick={handlingSubmit}>register</button>
+    </div>
+  )
+}
+
 
 export default App;
